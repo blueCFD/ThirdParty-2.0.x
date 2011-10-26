7423fd4272496aa7ffe0687238e4a18e3a8c617d
Bruno Santos <bruno.santos@bluecape.com.pt>
2011-10-26 08:27:03 +0100

    Updated versions for commented binutils defaults for mingw. Fixed mpich2-win file name definition for x86_64 -> x86-64.

--- etc/defaults.sh
+++ etc/defaults.sh
@@ -59,7 +59,7 @@ mingw32 | mingw-w32 | mingw-w64 | i686-w64-mingw32 | x86_64-w64-mingw32)
 #     gcc_version="gcc-4.4.6"
 #     gmp_version="gmp-4.3.2"
 #     mpfr_version="mpfr-2.4.2"
-#     binutils_version="binutils-2.21.1a"
+#     binutils_version="binutils-2.21"
 
     gcc_version="gcc-4.5.3"
     gmp_version="gmp-4.3.2"
@@ -71,7 +71,7 @@ mingw32 | mingw-w32 | mingw-w64 | i686-w64-mingw32 | x86_64-w64-mingw32)
 #    gmp_version=gmp-5.0.1
 #    mpfr_version=mpfr-2.4.2
 #    mpc_version=mpc-0.8.2
-#    binutils_version="binutils-2.21.1.1a"
+#    binutils_version="binutils-2.21"
 
     #MinGW versions
     mingw32rt_version="mingwrt-3.20"
